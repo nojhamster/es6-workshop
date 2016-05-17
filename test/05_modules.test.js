3853a25018d053cd9f8771151b086e33562570f7
Yannick Schurter <yannick.schurter@gmail.com>
2016-05-17 13:46:33 +0200

    put skip back

--- test/05_modules.test.js
+++ test/05_modules.test.js
@@ -7,7 +7,7 @@ import Mathy from '../common/Mathy';
 import {expect} from 'chai';
 
 describe('ES6 Modules', () => {
-  it('can import Mathy', () => {
+  it.skip('can import Mathy', () => {
 
     // I DID THIS ONE FOR YOU!
     expect(Mathy.sqrt).to.exist;
